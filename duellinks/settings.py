4f81f40f1605c9696b64694969fe77de9f79adef
fissionprime <fissionprime4@gmail.com>
2021-12-16 01:16:10 -0500

    changed database

--- duellinks/settings.py
+++ duellinks/settings.py
@@ -137,3 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
